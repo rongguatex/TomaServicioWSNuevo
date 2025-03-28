1e3226061314700710fcc2a1d6947e6885e51ee3
vannesaGTX <guatex.it.alyson.rodriguez@gmail.com>
2025-03-28 09:38:15 -0600

    detalle

--- src/java/com/guatex/tomaservicio/entidades/EDetalleGuia.java
+++ src/java/com/guatex/tomaservicio/entidades/EDetalleGuia.java
@@ -5,10 +5,34 @@
  */
 package com.guatex.tomaservicio.entidades;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  *
  * @author PJUNIOR-5
  */
+@XmlRootElement(name = "DETALLE_GUIA")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class EDetalleGuia {
     
+    @XmlElement(name = "LINEA_DETALLE_GUIA")
+    private  ELineaDetalleGuia linDetGuia;
+
+    public EDetalleGuia() {
+    }
+
+    public EDetalleGuia(ELineaDetalleGuia linDetGuia) {
+        this.linDetGuia = linDetGuia;
+    }
+
+    public ELineaDetalleGuia getLinDetGuia() {
+        return linDetGuia;
+    }
+
+    public void setLinDetGuia(ELineaDetalleGuia linDetGuia) {
+        this.linDetGuia = linDetGuia;
+    }
 }
